66d67d1055ce8883ce0191d5a247a1d9f4054c85
Gav Hern <contact@gavhern.com>
2021-02-19 13:19:53 -0800

    Add search api method

---
+++ public/api/search.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+
+Parameters for this file: q (the search query)
+
+*/
+
+
+
+// Requires (header information, database functions, authentication functions, utility functions)
+include 'tools/headers.php';
+include 'tools/db.php';
+include 'tools/auth.php';
+include 'tools/utils.php';
+
+
+// Get all nessisary parameters and sanitize them
+$values = array(
+    "user" => get_session(),
+    "query" => sanitize($_GET['q'])
+);
+
+// Get users that match the query
+$users = db("SELECT `id`, `name`, `username`, `profile_picture`, (SELECT COUNT(*) FROM `follows` WHERE follow = u.id) followers, (SELECT COUNT(*) FROM `follows` AS f WHERE f.follow = u.id AND f.user = {$values['user']}) is_following, u.`timestamp` FROM `users` AS u WHERE `username` LIKE '%{$values['query']}%' OR `name` LIKE '%{$values['query']}%' ORDER BY `timestamp` ASC LIMIT 50;", true);
+
+
+
+
+// Echo query results
+echo json_encode(array(
+    "success" => true,
+    "query" => $values['query'],
+    "users" => $users
+));
+
+/*
+
+FORMATTED SQL
+
+
+
+SELECT
+    `id`, `name`, `username`, `profile_picture`,
+    (SELECT COUNT(*) FROM `follows` WHERE follow = u.id) followers,
+    (SELECT COUNT(*) FROM `follows` AS f WHERE f.follow = u.id AND f.user = {$values['user']}) is_following,
+	u.`timestamp`
+FROM
+    `users` AS u
+WHERE
+    `username` LIKE '%{$values['query']}%' OR `name` LIKE '%{$values['query']}%'
+ORDER BY
+    `timestamp` ASC
+LIMIT 50
+
+
+*/
