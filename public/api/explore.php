65b42c9c1467a600cb02ce400d7e7f48c0a3de2c
Gav Hern <contact@gavhern.com>
2021-02-19 20:05:30 -0800

    Add explore tab

---
+++ public/api/explore.php
@@ -0,0 +1,70 @@
+<?php
+
+
+
+// Requires (header information, database functions, authentication functions, utility functions)
+include 'tools/headers.php';
+include 'tools/db.php';
+include 'tools/auth.php';
+include 'tools/utils.php';
+
+
+// Get all nessisary parameters and sanitize them
+$values = array(
+    "user" => get_session()
+);
+
+
+$recent = db("SELECT v.page, v.data AS id, u.name, u.username, u.profile_picture, MAX(v.timestamp) AS timestamp FROM `visits` AS v INNER JOIN `users` AS u ON v.data = u.id WHERE v.page = 'profile' AND v.user = 10 GROUP BY id ORDER BY timestamp DESC LIMIT 10;", true);
+
+
+$following = db("SELECT f.follow AS id, u.name, u.username, u.profile_picture, '1' AS is_following FROM `follows` AS f INNER JOIN users AS u ON f.follow = u.id WHERE `user` = {$values['user']} ORDER BY u.username DESC;", true);
+
+
+
+// Echo query results
+echo json_encode(array(
+    "success" => true,
+    "recent" => $recent,
+    "following" => $following
+));
+
+
+/*
+
+FORMATTED SQL
+
+
+
+Recent: 
+
+
+SELECT
+    v.page, v.data AS id, u.name, u.username, u.profile_picture, MAX(v.timestamp) AS timestamp
+FROM
+    `visits` AS v
+    INNER JOIN `users` AS u
+    ON v.data = u.id
+WHERE v.page = 'profile' AND v.user = 10
+GROUP BY id
+ORDER BY timestamp DESC
+LIMIT 10
+
+
+
+
+
+Following:
+
+
+SELECT
+    f.follow AS id, u.name, u.username, u.profile_picture, "1" AS is_following
+FROM
+    `follows` AS f
+    INNER JOIN users AS u
+    ON f.follow = u.id
+WHERE
+    `user` = 10
+
+
+*/
--- public/script.js
+++ public/script.js
@@ -1044,8 +1044,16 @@ const app = {
         let recentlyViewed = [];
         let userCards = [];
 
-        for(const i of data.following){
-          userCards.push(app.dom.components.userCard(i));
+        if(data.following.length != 0){
+          for(const i of data.following){
+            userCards.push(app.dom.components.userCard(i));
+          }
+        } else {
+          userCards.push({
+            tag: "div",
+            classes: ["w-full","flex","justify-center"],
+            html: '<div class="flex flex-col justify-center"><div class="flex justify-center mt-4 mb-1"><svg class="w-24 h-24 text-gray-300 dark:text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg></div><div class="font-semibold text-xl text-gray-400 dark:text-gray-600 text-center mb-2">You are not following anyone.</div></div>'
+          });
         }
 
         for(const i of data.recent){
@@ -1056,7 +1064,7 @@ const app = {
             children: [
               {
                 tag: 'img',
-                src: i.profile_picture,
+                src: (i.profile_picture=="") ? `https://socialmedia.gavhern.com/api/cdn.php?thumb&f=default` : `https://socialmedia.gavhern.com/api/cdn.php?thumb&f=${i.profile_picture}`,
                 classes: ["w-12","h-12","rounded-full","mb-2"]
               },
               {
@@ -1118,7 +1126,7 @@ const app = {
             },
             {
               tag: 'div',
-              classes: ["mx-4","my-6"],
+              classes: ["mx-4","my-6"].concat((data.recent.length == 0) ? ['hidden'] : []),
               children: [
                 {
                   tag: 'h1',
